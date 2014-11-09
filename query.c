4c5ac8aca1fa45d3e971627f37e69cdce5222fd5
Patrick Martin <patrickmmartin@gmail.com>
2014-11-09 16:00:47 +0000

    found correct preprocessor abuse to generate function names

--- query.c
+++ query.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-int query_imp(char * query)
+
+#define MAKE_FN_NAME(x) int query_type ## x (char * query)
+#define FUNCTION_NAME(y) MAKE_FN_NAME(y)
+
+FUNCTION_NAME(QUERY_TYPE)
 {
-  printf("query type QUERY_TYPE %s", query);
+  printf("query type %d %s", QUERY_TYPE, query);
 }
